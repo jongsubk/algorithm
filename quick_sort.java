86a5cd5d5d616adc29c100d1132826647693f9c3
jongsubk <jongsubkim@yahoo.com>
2019-10-23 20:14:33 +0900

    add swap function

--- quick_sort.java
+++ quick_sort.java
@@ -1,9 +1,25 @@
 import java.io.*;
 
-public class QuickSort {
+public class quick_sort {
 
   public static void main(String[] args) {
-    //System.out.println("Test");
+
+    int arr[] = { 3,2,1,4,5,9,8,7,6 };
+    printArray(arr);
   }
+
+  private static void printArray(int[] arr) {
+    for( int i=0; i<arr.length; i++) {
+      System.out.print(arr[i]+ " ");
+    }
+  }
+
+  private static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+
 
 }
