e585b64192b1a38487496fb386e6b72d74c453ed
jongs <jongsubkim@yahoo.com>
2020-11-20 13:34:30 -0600

    leetcode 1503_vector sort, get sumsum and compare

---
+++ 1403_Minimum_Subsequence_in_Non-Increasing_Order.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+
+vector<int> minSubsequence(vector<int>& nums) {
+    sort(nums.begin(), nums.end(), greater<>());
+    
+    vector<int> ret;
+
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    int subsum=0;
+                            
+    for(int i=0; i< nums.size(); i++) {
+        subsum += nums[i];
+        sum -= nums[i];
+        
+        ret.push_back(nums[i]);
+        if( subsum > sum ) {
+            return ret;
+        }
+    }
+    
+    return ret;
+}
+
+
+int main() {
+
+    vector<int> v = { 2,3,4,9,10,9};
+
+    vector<int> result = minSubsequence(v);
+
+    for(int value : result){
+        cout << value << " ";
+    }
+
+}
